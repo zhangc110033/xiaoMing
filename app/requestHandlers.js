17aeca7d058274a499b0303b61cef85d02f95696
zhangc110033 <zhangc110033@hotmail.com>
2014-01-31 18:59:33 +0800

    format

--- app/requestHandlers.js
+++ app/requestHandlers.js
@@ -1,8 +1,17 @@
 var querystring = require("querystring"), fs = require("fs"), formidable = require("formidable");
 function start(response) {
 	console.log("Request handler 'start' was called.");
-	var body = '<html>' + '<head>' + '<meta http-equiv="Content-Type" content="text/html; ' + 'charset=UTF-8" />' + '</head>' + '<body>' + '<form action="/upload" enctype="multipart/form-data" ' + 'method="post">' + '<input type="file" name="upload" multiple="multiple">' + '<input type="submit" value="Upload file" />' + '</form>' + '</body>' + '</html>';
-	response.writeHead(200, { "Content-Type" : "text/html" });
+	var body = '<html>' + '<head>'
+			+ '<meta http-equiv="Content-Type" content="text/html; '
+			+ 'charset=UTF-8" />' + '</head>' + '<body>'
+			+ '<form action="/upload" enctype="multipart/form-data" '
+			+ 'method="post">'
+			+ '<input type="file" name="upload" multiple="multiple">'
+			+ '<input type="submit" value="Upload file" />' + '</form>'
+			+ '</body>' + '</html>';
+	response.writeHead(200, {
+		"Content-Type" : "text/html"
+	});
 	response.write(body);
 	response.end();
 }
@@ -13,7 +22,9 @@ function upload(response, request) {
 	form.parse(request, function(error, fields, files) {
 		console.log("parsing done");
 		fs.renameSync(files.upload.path, "/tmp/test.png");
-		response.writeHead(200, { "Content-Type" : "text/html" });
+		response.writeHead(200, {
+			"Content-Type" : "text/html"
+		});
 		response.write("received image:<br/>");
 		response.write("<img src='/show' />");
 		response.end();
@@ -23,11 +34,15 @@ function show(response) {
 	console.log("Request handler 'show' was called.");
 	fs.readFile("/tmp/test.png", "binary", function(error, file) {
 		if (error) {
-			response.writeHead(500, { "Content-Type" : "text/plain" });
+			response.writeHead(500, {
+				"Content-Type" : "text/plain"
+			});
 			response.write(error + "\n");
 			response.end();
 		} else {
-			response.writeHead(200, { "Content-Type" : "image/png" });
+			response.writeHead(200, {
+				"Content-Type" : "image/png"
+			});
 			response.write(file, "binary");
 			response.end();
 		}
